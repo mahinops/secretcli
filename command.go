2553eec05d5e85ff1f9f790071d4d43053caa766
Mokhlesur Mahin <mokhlesurr031@gmail.com>
2024-10-04 13:33:15 +0600

    update secrets working

--- command.go
+++ command.go
@@ -6,12 +6,14 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 type CmdFlags struct {
 	Add    bool
 	List   bool
 	Delete int
+	Edit   int
 }
 
 // NewCommandFlags initializes command flags
@@ -20,6 +22,7 @@ func NewCommandFlags() *CmdFlags {
 	flag.BoolVar(&cf.Add, "add", false, "Add a new secret")
 	flag.BoolVar(&cf.List, "list", false, "List all secrets")
 	flag.IntVar(&cf.Delete, "del", -1, "Delete a secrets by index")
+	flag.IntVar(&cf.Edit, "edit", -1, "Edit a secret by index")
 	flag.Parse()
 	return &cf
 }
@@ -32,8 +35,8 @@ func (cf *CmdFlags) Execute(secrets *Secrets) {
 		secrets.list()
 	case cf.Delete != -1:
 		secrets.delete(cf.Delete)
-	// case cf.Edit != -1:
-	// 	cf.editSecret(secrets, cf.Edit)
+	case cf.Edit != -1:
+		cf.editSecret(secrets, cf.Edit)
 	default:
 		fmt.Println("Invalid Command. Use --help to see available commands.")
 	}
@@ -89,4 +92,69 @@ func (cf *CmdFlags) addSecret(secrets *Secrets) {
 		return
 	}
 	fmt.Println("Secret added successfully!")
+}
+
+// Edit an existing secret
+func (cf *CmdFlags) editSecret(secrets *Secrets, index int) {
+	// Fetch the existing secret
+	secret := (*secrets)[index]
+
+	fmt.Println("Editing secret:", secret.Title)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Println("Select field to edit:")
+		fmt.Println("1. Title")
+		fmt.Println("2. Username")
+		fmt.Println("3. Password")
+		fmt.Println("4. Note")
+		fmt.Println("5. Email")
+		fmt.Println("6. Website")
+		fmt.Println("7. Save and Exit")
+		fmt.Print("Enter your choice (1-7): ")
+
+		if scanner.Scan() {
+			choice := scanner.Text()
+			switch choice {
+			case "1":
+				fmt.Print("Enter new Title: ")
+				scanner.Scan()
+				secret.Title = scanner.Text()
+			case "2":
+				fmt.Print("Enter new Username: ")
+				scanner.Scan()
+				secret.Username = scanner.Text()
+			case "3":
+				fmt.Print("Enter new Password: ")
+				scanner.Scan()
+				secret.Password, _ = encrypt(scanner.Text())
+			case "4":
+				fmt.Print("Enter new Note: ")
+				scanner.Scan()
+				secret.Note = scanner.Text()
+			case "5":
+				fmt.Print("Enter new Email: ")
+				scanner.Scan()
+				secret.Email = scanner.Text()
+			case "6":
+				fmt.Print("Enter new Website: ")
+				scanner.Scan()
+				secret.Website = scanner.Text()
+			case "7":
+				secret.UpdatedAt = new(time.Time)
+				*secret.UpdatedAt = time.Now()
+				(*secrets)[index] = secret // Update the secret in the slice
+				fmt.Println("Secret updated successfully!")
+				return
+			default:
+				fmt.Println("Invalid choice. Please try again.")
+			}
+		}
+
+		// Check for errors
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Error reading input:", err)
+			return
+		}
+	}
 }
