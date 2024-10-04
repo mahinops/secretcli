6e39b2288e72f20d2cd2b2cfe16628a2c11c39b8
Mokhlesur Mahin <mokhlesurr031@gmail.com>
2024-10-04 13:19:34 +0600

    fix: add, multiple word input

--- command.go
+++ command.go
@@ -2,12 +2,14 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"os"
 )
 
 type CmdFlags struct {
-	Add    string
+	Add    bool
 	List   bool
 	Delete int
 }
@@ -15,7 +17,7 @@ type CmdFlags struct {
 // NewCommandFlags initializes command flags
 func NewCommandFlags() *CmdFlags {
 	cf := CmdFlags{}
-	flag.StringVar(&cf.Add, "title", "", "Add a new secret")
+	flag.BoolVar(&cf.Add, "add", false, "Add a new secret")
 	flag.BoolVar(&cf.List, "list", false, "List all secrets")
 	flag.IntVar(&cf.Delete, "del", -1, "Delete a secrets by index")
 	flag.Parse()
@@ -25,27 +27,57 @@ func NewCommandFlags() *CmdFlags {
 // Execute processes the command flags and prompts for secret details
 func (cf *CmdFlags) Execute(secrets *Secrets) {
 	switch {
-	case cf.Add != "":
-		var username, password, note, email, website string
+	case cf.Add:
+		var title, username, password, note, email, website string
 		fmt.Println("Adding a new secret with title:", cf.Add)
 
+		// Create a new scanner
+		scanner := bufio.NewScanner(os.Stdin)
+
+		// Input Title
+		fmt.Print("Enter Title: ")
+		if scanner.Scan() {
+			title = scanner.Text()
+		}
+
+		// Input Username
 		fmt.Print("Enter Username: ")
-		fmt.Scanln(&username)
+		if scanner.Scan() {
+			username = scanner.Text()
+		}
 
+		// Input Password
 		fmt.Print("Enter Password: ")
-		fmt.Scanln(&password)
+		if scanner.Scan() {
+			password = scanner.Text()
+		}
 
+		// Input Note (optional)
 		fmt.Print("Enter Note (optional): ")
-		fmt.Scanln(&note)
+		if scanner.Scan() {
+			note = scanner.Text()
+		}
 
+		// Input Email (optional)
 		fmt.Print("Enter Email (optional): ")
-		fmt.Scanln(&email)
+		if scanner.Scan() {
+			email = scanner.Text()
+		}
 
+		// Input Website (optional)
 		fmt.Print("Enter Website (optional): ")
-		fmt.Scanln(&website)
+		if scanner.Scan() {
+			website = scanner.Text()
+		}
+
+		// Check for errors
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Error reading input:", err)
+			return
+		}
 
 		// Call the add method to add the new secret
-		err := secrets.add(cf.Add, username, password, note, email, website)
+		err := secrets.add(title, username, password, note, email, website)
 		if err != nil {
 			fmt.Println("Error adding secret:", err)
 			return
